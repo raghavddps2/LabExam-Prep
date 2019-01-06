0c8815775471cfd4d726bb80ede0c619e2366b80
raghavddps2 <raghav.ddps2@gmail.com>
2019-01-06 18:13:16 +0530

    Added binary tree

---
+++ LabExam/Dslab/BinaryTree.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<ctype.h>
+struct node{
+    struct node *left;
+    int data;
+    struct node *right;
+};
+struct node *root = NULL;
+struct node *tree = NULL;
+
+struct node *createNode(int data){
+    if(data == -1){
+        return NULL;
+    }
+    struct node *newNode;
+    newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void createTree(struct node *root){
+    int data;
+    if(root == NULL){
+        printf("\nEnter the value of the root element:\t");
+        scanf("%d",&data);
+        root = createNode(data);
+        tree = root;
+    }
+    printf("\nThe value of the root element is %d",root->data);
+
+    printf("\nEnter the value of the left child. Enter -1 to put NULL:\t");
+    scanf("%d",&data);
+    root->left = createNode(data);
+
+    printf("\nEnter the value of the right child. Enter -1 to put NULL:\t");
+    scanf("%d",&data);
+    root->right = createNode(data);
+
+    if(root->right != NULL){
+        createTree(root->right);
+    }
+    if(root->left != NULL){
+        createTree(root->left);
+    }
+}
+void inOrderTraversal(struct node *root){
+    if(root != NULL){
+        inOrderTraversal(root->left);
+        printf("%d ",root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+
+void preOrderTraversal(struct node *root){
+    if(root != NULL){
+        preOrderTraversal(root->left);
+        preorderTraversal(root->right);
+        printf("%d ",root->data);
+    }
+}
+
+
+void postOrderTraversal(struct node *root){
+    if(root != NULL){
+        postOrderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ",root->data);
+    }
+}
+
+void main(){
+    printf("\nCreating the tree");
+    createTree(tree);
+    printf("\n Inorder Traversal");
+    inorderTraversal(tree);
+    
+    printf("\n Preorder Traversal");
+    PreorderTraversal(tree);
+    
+    printf("\n Postorder Traversal");
+    PostorderTraversal(tree);
+}
